eb479a428dcc48a751af8937596367e08dc6dc66
Davi Silva <davisilvaphoto@gmail.com>
2025-11-27 17:31:42 -0300

    remove theme reference

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,9 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require("lualine").setup({
-      options = {},
+      options = {
+        theme = "onenord",
+      },
     })
   end,
 }
