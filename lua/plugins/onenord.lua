560b8c731688b0d291f482d9a1c790de5ef3c3ab
Davi Silva <davisilvaphoto@gmail.com>
2025-11-27 17:13:02 -0300

    adjust theme

--- lua/plugins/onenord.lua
+++ lua/plugins/onenord.lua
@@ -1,8 +1,10 @@
 return {
   "rmehri01/onenord.nvim",
   name = "onenord",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require("onenord").setup()
-    vim.colorscheme = "onenord"
+    vim.cmd("colorscheme onenord")
   end,
 }
